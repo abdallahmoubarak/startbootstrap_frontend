669d1dd3d5821801633a8d7946f856ffe52728b3
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-12 21:45:15 +0300

    popUp functioning without changing images

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+  //   menu
   const menuBtn = document.getElementById("menu-btn");
   const menuItems = document.getElementById("menu-items");
 
@@ -9,4 +10,22 @@ window.addEventListener("DOMContentLoaded", () => {
       menuItems.style.display = "flex";
     }
   });
+
+  //   popup
+  const cards = document.getElementsByClassName("card");
+  const closeBtn = document.getElementById("popup-close-btn");
+  const close = document.getElementById("popup-close");
+  const popUp = document.getElementById("popup");
+
+  for (var i = 0; i <= cards.length; i++) {
+    cards[i].addEventListener("click", () => {
+      popUp.style.display = "block";
+      closeBtn.addEventListener("click", () => {
+        popUp.style.display = "none";
+      });
+      close.addEventListener("click", () => {
+        popUp.style.display = "none";
+      });
+    });
+  }
 });
